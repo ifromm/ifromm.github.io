f5d04751434d786819b0de0e4952d143b7a5f739
Ingo Frommholz <ingo@frommholz.org>
2024-02-10 23:08:06 +0000

    Add files via upload

---
+++ banner.js
@@ -0,0 +1,17 @@
+var banner = document.getElementById("banner");
+//var sticky = banner.offsetTop;
+var sticky = 400;
+
+function fixedBanner() {
+  if (window.pageYOffset >= sticky) {
+	document.getElementById("profilepic").src = "ingo_202107.png";
+	document.getElementById("banner").classList.add("sticky")
+	document.getElementById("profilepic").classList.add("profilepic2")
+	document.getElementById("address").classList.add("address2")
+  } else {
+	document.getElementById("profilepic").src = "ingo_202107.png";
+	document.getElementById("banner").classList.remove("sticky");
+	document.getElementById("profilepic").classList.remove("profilepic2")
+	document.getElementById("address").classList.remove("address2")
+  }
+}
