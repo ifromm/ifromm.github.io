7e2bd11d447517aea1d47c897a7822566f56b85b
Ingo Frommholz <ingo@frommholz.org>
2024-11-02 19:31:08 +0000

    New pic

--- banner.js
+++ banner.js
@@ -4,12 +4,12 @@ var sticky = 400;
 
 function fixedBanner() {
   if (window.pageYOffset >= sticky) {
-	document.getElementById("profilepic").src = "ingo_202107.png";
+	document.getElementById("profilepic").src = "ingo_may2024.png";
 	document.getElementById("banner").classList.add("sticky")
 	document.getElementById("profilepic").classList.add("profilepic2")
 	document.getElementById("address").classList.add("address2")
   } else {
-	document.getElementById("profilepic").src = "ingo_202107.png";
+	document.getElementById("profilepic").src = "ingo_may2024.png";
 	document.getElementById("banner").classList.remove("sticky");
 	document.getElementById("profilepic").classList.remove("profilepic2")
 	document.getElementById("address").classList.remove("address2")
